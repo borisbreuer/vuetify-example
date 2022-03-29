91c9310954c924bb1550e4e98c4b8fe06fc65da6
Boris Breuer <boris.breuer@tivian.com>
2022-03-29 10:50:28 +0200

    initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,153 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <v-container>
+        <v-row>
+            <v-col cols="12">
+                <h1>Autocomplete</h1>
+                <v-row class="mt-2" justify="center">
+                    <v-autocomplete
+                        v-model="values"
+                        :items="items"
+                        outlined
+                        dense
+                        chips
+                        small-chips
+                        deletable-chips
+                        label="Name"
+                    ></v-autocomplete>
+                </v-row>
+            </v-col>
+
+            <v-col class="mb-4">
+                <v-row justify="center">
+                    <v-progress-circular
+                        :rotate="360"
+                        :size="100"
+                        :width="15"
+                        :value="45.9"
+                        color="teal"
+                    >
+                        45.9%
+                    </v-progress-circular>
+
+                    <v-progress-circular
+                        :rotate="-90"
+                        :size="100"
+                        :width="15"
+                        :value="value"
+                        color="primary"
+                    >
+                        {{ value }}
+                    </v-progress-circular>
+
+                    <v-progress-circular
+                        :rotate="90"
+                        :size="100"
+                        :width="15"
+                        :value="value"
+                        color="red"
+                    >
+                        {{ value }}
+                    </v-progress-circular>
+
+                    <v-progress-circular
+                        :rotate="180"
+                        :size="100"
+                        :width="15"
+                        :value="value"
+                        color="pink"
+                    >
+                        {{ value }}
+                    </v-progress-circular>
+                </v-row>
+            </v-col>
+
+            <v-col class="mb-5" cols="12">
+                <v-row justify="center"> </v-row>
+            </v-col>
+
+            <v-col class="mb-5" cols="12">
+                <v-row justify="center">
+                    <v-card width="500">
+                        <div class="d-flex flex-row pt-1">
+                            <div>
+                                <v-sheet
+                                    rounded
+                                    class="d-flex justify-center align-center ma-2"
+                                    height="50"
+                                    width="50"
+                                    color="primary"
+                                >
+                                    <v-icon m-large color="white">mdi-whistle</v-icon>
+                                </v-sheet>
+                            </div>
+                            <div>
+                                <v-card-title class="mt-0 pt-0"> Top western road trips </v-card-title>
+                                <v-card-subtitle> 1,000 miles of wonder </v-card-subtitle>
+                            </div>
+                        </div>
+
+                        <v-card-actions>
+                            <v-btn color="primary" outlined elevation="0"> Explore </v-btn>
+
+                            <v-spacer></v-spacer>
+
+                            <v-btn icon @click="show = !show">
+                                <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+                            </v-btn>
+                        </v-card-actions>
+
+                        <v-expand-transition>
+                            <div v-show="show">
+                                <v-divider></v-divider>
+
+                                <v-card-text>
+                                    I'm a thing. But, like most politicians, he promised more than
+                                    he could deliver. You won't have time for sleeping, soldier, not
+                                    with all the bed making you'll be doing. Then we'll go with that
+                                    data file! Hey, you add a one and two zeros to that or we walk!
+                                    You're going to do his laundry? I've got to find a way to
+                                    escape.
+                                </v-card-text>
+                            </div>
+                        </v-expand-transition>
+                    </v-card>
+                </v-row>
+            </v-col>
+
+            <v-col class="mb-5" cols="12">
+                <v-row justify="center"> </v-row>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    name: 'HelloWorld',
+
+    data: () => ({
+        items: ['Estelle', 'Francesco', 'Marcello', 'Boris'],
+        values: [],
+        show: false,
+        interval: {},
+        value: 0,
+    }),
+    beforeDestroy() {
+        clearInterval(this.interval);
+    },
+    mounted() {
+        this.interval = setInterval(() => {
+            if (this.value === 100) {
+                return (this.value = 0);
+            }
+            this.value += 10;
+        }, 1000);
+    },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.v-progress-circular {
+    margin: 1rem;
 }
 </style>
